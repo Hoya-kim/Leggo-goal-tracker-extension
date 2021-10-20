8c9513f27e7be5298ae6e49be004ddf4735e1de2
TaejoonPark <joker77z@naver.com>
2021-10-20 15:34:45 +0900

    Sync login and navbar (#20)
    * feat: fixed FOIT
    
    깜빡이는 현상을 해결했습니다.
    
    * feat: edit min-width and nav position
    
    전반적으로 수정했습니다.
    
    * feat: add dynamic nickname
    
    add nickname
    
    * feat: Sync login and navbar
    
    아바타 연동

--- js/Nav.js
+++ js/Nav.js
@@ -1,5 +1,8 @@
+// TODO:
+// - 순서 교정하기
+
 let state = {
-  isNavigationOpended: true,
+  isNavigationOpend: false,
   isInitRender: false,
 };
 
@@ -7,30 +10,61 @@ const $toggleButton = document.querySelector('.toggle');
 const $headerProfile = document.querySelector('.header-profile');
 const $navContainer = document.querySelector('.nav-container');
 const $nav = document.querySelector('nav');
+const $avatarFace = document.querySelector('.avatar-face');
+const $avatarEyes = document.querySelector('.avatar-eyes');
+const $avatarMouth = document.querySelector('.avatar-mouth');
+const $navNickname = document.querySelector('.nav-nickname');
+const $goalContainer = document.querySelector('.goal-container');
 
 const render = () => {
-  state.isNavigationOpended === true
+  state.isNavigationOpend === true
     ? ($headerProfile.style.opacity = '0')
     : ($headerProfile.style.opacity = '1');
-  $nav.classList.toggle('active', state.isNavigationOpended);
-  // $navContainer.classList.toggle('active', state.isInitRender);
+  $nav.classList.toggle('active', state.isNavigationOpend);
+  [$navContainer, $headerProfile, $toggleButton, $goalContainer].forEach($el =>
+    $el.classList.toggle('notransition', state.isInitRender),
+  );
+
+  // 아바타
+  document.documentElement.style.setProperty(
+    '--face-color',
+    `var(${JSON.parse(localStorage.getItem('userInfo')).color.face})`,
+  );
+  document.documentElement.style.setProperty(
+    '--face-border-color',
+    `var(${JSON.parse(localStorage.getItem('userInfo')).color.border})`,
+  );
+
+  const { eyes } = JSON.parse(localStorage.getItem('userInfo'));
+  $avatarEyes.style['background-image'] = 'url(../images/avatar/' + eyes;
+
+  const { mouth } = JSON.parse(localStorage.getItem('userInfo'));
+  $avatarMouth.style['background-image'] = 'url(../images/avatar/' + mouth;
+
+  // 닉네임
+  $navNickname.innerHTML = JSON.parse(localStorage.getItem('userInfo')).nickname;
 };
 
 const setState = newState => {
   state = newState;
-  localStorage.setItem('isNavigationOpended', state.isNavigationOpended);
+  sessionStorage.setItem('isNavigationOpend', state.isNavigationOpend);
   render();
 };
 
 $toggleButton.addEventListener('click', () => {
-  setState({ isNavigationOpended: !state.isNavigationOpended, isInitRender: false });
+  setState({ isNavigationOpend: !state.isNavigationOpend, isInitRender: false });
+});
+
+window.addEventListener('click', e => {
+  if (e.key === '2') {
+    console.log('tset');
+  }
 });
 
 window.addEventListener('DOMContentLoaded', () => {
-  if (localStorage.getItem('isNavigationOpended')) {
-    const isNavigationOpended = JSON.parse(localStorage.getItem('isNavigationOpended')) || false;
-    setState({ isNavigationOpended, isInitRender: true });
-  } else {
-    setState({ isNavigationOpended: true, isInitRender: true });
-  }
+  setTimeout(() => {
+    document.body.style.opacity = 1;
+  }, 300);
+  const isNavigationOpend = JSON.parse(sessionStorage.getItem('isNavigationOpend')) || false;
+  setState({ isNavigationOpend, isInitRender: true });
 });
