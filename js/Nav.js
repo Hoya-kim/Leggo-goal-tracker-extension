83363551dfb60a7e37b6da87b31f267c8deb090a
TaejoonPark <joker77z@naver.com>
2021-10-21 14:38:34 +0900

    feat: Edit menu bar markup and style (#42)
    Some modifications to the style

--- js/Nav.js
+++ js/Nav.js
@@ -1,5 +1,3 @@
-// TODO:
-// - 순서 교정하기
 const IMAGE_SPRITE_NUMBERS_OF_EYES = 6;
 const IMAGE_SPRITE_NUMBERS_OF_MOUTH = 5;
 
@@ -18,6 +16,7 @@ const $avatarMouth = document.querySelectorAll('.avatar-mouth');
 const $navNickname = document.querySelector('.nav-nickname');
 const $goalContainer = document.querySelector('.goal-container');
 const $navMenuList = document.querySelector('.nav-menu-list');
+const $navMenuCommingsoon = document.querySelector('.nav-menu-commingsoon');
 
 const render = () => {
   state.isNavigationOpend === true
@@ -81,10 +80,18 @@ window.addEventListener('keyup', e => {
 });
 
 $navMenuList.addEventListener('click', e => {
-  if (!e.target.classList.contains('nav-menu')) return;
+  if (
+    !e.target.classList.contains('nav-menu') ||
+    e.target.classList.contains('nav-menu-commingsoon')
+  )
+    return;
   [...document.querySelectorAll('.nav-menu')].forEach($menu => {
     $menu.classList.toggle('nav-menu-select', $menu === e.target);
   });
+});
+
+$navMenuCommingsoon.addEventListener('click', () => {
+  alert('comming soon..');
 });
 
 window.addEventListener('DOMContentLoaded', () => {
